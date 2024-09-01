3d80b5bf1a6c23a058f07baffb785d5a20219cb7
Zephuris <amirreza.hsi@yahoo.com>
2024-09-01 10:44:47 +0330

    create post and comment model

---
+++ DomainLayer/CommentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class CommentModel : EntityBlogModel
+    {
+        public int PostId { get; set; }
+        public string content { get; set; }
+        public string author { get; set; }
+
+    }
+}
