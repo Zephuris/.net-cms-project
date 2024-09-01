c54ddaa9ebe9bbc7325c67e0c38e984523f9ed9a
Zephuris <amirreza.hsi@yahoo.com>
2024-09-01 10:55:18 +0330

    Create-Interface&Repo

---
+++ InfrastructureLayer/BlogRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly IDatabaseConnection _databaseConnection;
+        CustomActionResult _result;
+
+        public BlogRepository(IDatabaseConnection databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+            _result = new CustomActionResult();
+        }
+
+        public async Task<CustomActionResult> CreateBlog(PostModel model)
+        {
+            try
+            {
+                CustomActionResult<System.Data.IDbConnection> connection = await _databaseConnection.GetConnection();
+                _result.IsSuccess = connection.IsSuccess;
+                _result.Message = connection.Message;
+                if (!_result.IsSuccess) return _result;
+
+                var command = "prc_create_blog";
+
+
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add(name: "@title", value: model.Title);
+                parameters.Add(name: "@body", value: model.Body);
+
+
+                await connection.Data.ExecuteAsync(command, parameters, commandType: System.Data.CommandType.StoredProcedure);
+                _result.IsSuccess = true;
+                _result.Message = "Contact Created Successfully.";
+
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException();
+
+            }
+            return _result;
+
+        }
+
+        public async Task<bool> DeleteBlog(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+        public async Task<PostModel> GetBlogById(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+        public async Task<bool> UpdateBlog(PostModel model)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<CustomActionResult<List<PostModel>>> GetBlogs()
+        {
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+    }
+}
