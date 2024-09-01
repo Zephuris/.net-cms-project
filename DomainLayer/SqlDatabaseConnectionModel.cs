c2f54a043a340c894247acd346093a1324a37659
Zephuris <amirreza.hsi@yahoo.com>
2024-09-01 10:47:21 +0330

    create database models

---
+++ DomainLayer/SqlDatabaseConnectionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class SqlDatabaseConnectionModel : DatabaseConnectionModel
+    {
+        public string ConnectionString { get { return @$"Server= {servername}; Database={databaseName}; Trusted_Connection=True"; } }
+    }
+}
