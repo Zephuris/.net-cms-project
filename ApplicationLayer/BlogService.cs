57478d50cda4a7e8b039efe6dcb446120567e5fd
Zephuris <amirreza.hsi@yahoo.com>
2024-09-01 10:57:59 +0330

    Create-ApplicationLayerAndBlogService

---
+++ ApplicationLayer/BlogService.cs
@@ -0,0 +1,45 @@
+﻿using DomainLayer;
+using InfrastructureLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IBlogRepository _respository;
+
+        public BlogService(IBlogRepository respository)
+        {
+            _respository = respository;
+        }
+
+        public async Task<CustomActionResult> CreateBlog(PostModel model)
+        {
+            return await _respository.CreateBlog(model);
+
+        }
+        public Task<bool> DeleteBlog(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostModel> UpdateBlog(PostModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostModel> GetBlogById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CustomActionResult<List<PostModel>>> GetBlogs()
+        {
+            return await _respository.GetBlogs();
+        }
+    }
+}
---
+++ ApplicationLayer/BlogServiceInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer
+{
+    public interface IBlogService
+    {
+        Task<CustomActionResult> CreateBlog(PostModel model);
+        Task<CustomActionResult<List<PostModel>>> GetBlogs();
+        Task<PostModel> GetBlogById(int id);
+        Task<PostModel> UpdateBlog(PostModel model);
+        Task<bool> DeleteBlog(int ids);
+
+    }
+}
